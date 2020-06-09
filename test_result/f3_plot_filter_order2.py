01a5d012c5b989ed432a2ea346b123ddc58cf9cc
ddthuan <59833376+ddthuan@users.noreply.github.com>
2020-06-09 10:35:29 +0800

    Create f3_plot_filter_order2.py

---
+++ test_result/f3_plot_filter_order2.py
@@ -0,0 +1,69 @@
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+
+def plot_filters_single_channel(t, name):
+    
+    #kernels depth * number of kernels
+    nplots = t.shape[0]*t.shape[1]
+    ncols = 12
+    
+    nrows = 1 + nplots//ncols
+    #convert tensor to numpy image
+    npimg = np.array(t.numpy(), np.float32)
+    
+    count = 0
+    fig = plt.figure(figsize=(ncols, nrows))
+    
+    #looping through all the kernels in each channel
+    for i in range(t.shape[0]):
+        for j in range(t.shape[1]):
+            count += 1
+            ax1 = fig.add_subplot(nrows, ncols, count)
+            npimg = np.array(t[i, j].numpy(), np.float32)
+            npimg = (npimg - np.mean(npimg)) / np.std(npimg)
+            npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
+            ax1.imshow(npimg)
+            ax1.set_title(str(i) + ',' + str(j))
+            ax1.axis('off')
+            ax1.set_xticklabels([])
+            ax1.set_yticklabels([])
+   
+    plt.tight_layout()
+    plt.savefig(name)
+    plt.show()
+    
+from model_test import Scatt_TwoOrder
+
+model = Scatt_TwoOrder(8,1,[2,2])
+#model = Scatt_TwoOrder(8,1,[2,2])
+model.to("cuda:0")
+model_name = model.__class__.__name__
+
+#Ok, random
+#PATH_random = './model/Scatt_OneOrder_random_level1_Smooth_epsConstant.pth'
+
+#0k, no eps, cifar10
+#PATH_cifar = './model/Scatt_OneOrder_cifar10_level1_Smooth_best.pth'
+
+#Ok, tiny restnet
+PATH_imagenet = './model/Scatt_TwoOrder_imagenet_level2_Smooth_200.pth'
+PATH_imagenet_none = './model/Scatt_TwoOrder_imagenet_level2_None_Smooth_200.pth'
+PATH_cifar100 = './model/Scatt_TwoOrder_cifar100_level2_None_Smooth_60.pth'
+#names = ['random', 'cifar', 'imagenet']
+#paths = [PATH_random, PATH_cifar, PATH_imagenet]
+
+names = ['cifar100',]
+paths = [PATH_cifar100]
+
+for NAME, PATH in zip(names, paths):
+    
+    
+    # Load pre-traing model
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+    
+    plot_filters_single_channel(model.phi.weight.detach().cpu(), "plot/order2/s1_{}_phi".format(NAME))
+    plot_filters_single_channel(model.psi_real.weight.detach().cpu(), "plot/order2/s1_{}_psi_real".format(NAME))
+    plot_filters_single_channel(model.psi_imag.weight.detach().cpu(), "plot/order2/s1_{}_psi_imag".format(NAME))
+
